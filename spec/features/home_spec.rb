4ad83c2a256f1ad0c1b714ec405be193bcbf7670
Joseph W Becher <jwbecher@drazisil.com>
2018-05-29 12:46:49 -0400

    Test for canvas and take screenshot

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+class Home
+  include Capybara::DSL
+  def visit_homepage
+    Capybara.app = Rack::File.new('.')
+    visit("index.html")
+  end
+end
+feature "Visit homepage" do
+  let(:home) {Home.new}
+  scenario "Able to see text, Word Nerds", :js => true do
+    home.visit_homepage
+    page.save_screenshot('screenshot.png')
+    expect(page).to have_xpath("//canvas")
+  end
+end
